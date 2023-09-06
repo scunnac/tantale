f26337f0e22e1820e2b54d832d64e0c531fd5352
cunnac <sebastien.cunnac@ird.fr>
2023-09-06 21:15:24 +0200

    Remove dependency on ggmsa

--- R/target_predictions.R
+++ R/target_predictions.R
@@ -290,8 +290,8 @@ plotTaleTargetPred <- function(predResults, subjDnaSeqFile, filterRange) {
   tidySubjSeqs <- lapply(1:length(subjDnaSeqs),
                          function(i) {
                            oneSeq <- subjDnaSeqs[i]
-                           dplyr::bind_rows(`+` = ggmsa::tidy_msa(oneSeq),
-                                            `-` = ggmsa::tidy_msa(Biostrings::complement(oneSeq)),
+                           dplyr::bind_rows(`+` = tidy_biostrings_msa(oneSeq),
+                                            `-` = tidy_biostrings_msa(Biostrings::complement(oneSeq)),
                                             .id = "strand") %>%
                              dplyr::as_tibble() %>%
                              dplyr::mutate(yPos = dplyr::if_else(strand == "+",
@@ -444,6 +444,51 @@ plotTaleTargetPred <- function(predResults, subjDnaSeqFile, filterRange) {
 }
 
 
+
+
+##' Convert msa file/object to tidy data frame.
+##'
+##'
+##' @title tidy_msa
+##' @param msa multiple sequence alignment file or sequence object in 
+##' DNAStringSet, RNAStringSet, AAStringSet, BStringSet, DNAMultipleAlignment, 
+##' RNAMultipleAlignment, AAMultipleAlignment, DNAbin or AAbin
+##' @param start start position to extract subset of alignment
+##' @param end end position to extract subset of alignemnt
+##' @author Modified from Guangchuang Yu
+##' @noRd
+tidy_biostrings_msa <- function(msa, start = NULL, end = NULL) {
+  aln <- msa
+  alnmat <- lapply(seq_along(aln), function(i) {
+    ##Preventing function collisions
+    base::strsplit(as.character(aln[[i]]), '')[[1]]
+  }) %>% do.call('rbind', .)
+  ## for DNAbin and AAbin
+  alndf <- as.data.frame(alnmat, stringsAsFactors = FALSE)
+  
+  if(unique(names(aln)) %>% length == length(aln)) {
+    alndf$name = names(aln)
+  }else{
+    stop("Sequences must have unique names")
+  }
+  cn = colnames(alndf)
+  cn <- cn[!cn %in% "name"]
+  df <- gather(alndf, "position", "character", cn)
+  
+  y <- df
+  y$position = as.numeric(sub("V", "", y$position))
+  y$character = toupper(y$character)
+  
+  y$name = factor(y$name, levels=rev(names(aln)))
+  
+  
+  if (is.null(start)) start <- min(y$position)
+  if (is.null(end)) end <- max(y$position)
+  
+  y <- y[y$position >=start & y$position <= end, ]
+  
+  return(y)
+}
 
 
 
